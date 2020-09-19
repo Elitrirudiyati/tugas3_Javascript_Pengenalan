0f841e3691c58475b1d751c41746336f9c310260
Elitrirudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-09-20 06:52:44 +0700

    Add files via upload

---
+++ tugas3.js
@@ -0,0 +1,17 @@
+
+function kali(){
+	hasilKali=a*b;
+	return hasilKali;
+}
+
+var a = 5;
+var b=6;
+var hasilperkalian=kali(a, b);
+var a=10
+
+console.log(hasilperkalian);
+console.log("Setelah diubah nilai",a, "menjadi",kali());
+
+
+
+
